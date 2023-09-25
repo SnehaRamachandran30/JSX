e6c164f3f9c359388958ba92d31b610cd820d252
SnehaRamachandran30 <145957625+SnehaRamachandran30@users.noreply.github.com>
2023-09-25 13:11:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 var React = require("react");
 var ReactDom = require("react-dom");
-ReactDom.render(<h1>SnehaGogul</h1>, document.getElementById("root"));
+ReactDom.render(<h1>Sneha</h1>, document.getElementById("root"));
